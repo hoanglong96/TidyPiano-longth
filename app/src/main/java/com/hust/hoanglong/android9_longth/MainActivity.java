6b726d3b5e09da85c1ba96aedca253866166cc16
HOANGLONG-PC\HoangLong <long11hq@gmail.com>
2017-05-28 10:57:18 +0700

    Selector press button. But Fail

--- app/src/main/java/com/hust/hoanglong/android9_longth/MainActivity.java
+++ app/src/main/java/com/hust/hoanglong/android9_longth/MainActivity.java
@@ -3,11 +3,45 @@ package com.hust.hoanglong.android9_longth;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView white1,white2,white3,white4,white5,white6,white7;
+    private ImageView black1,black2,black3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControl();
+        addEvents();
+    }
+
+    private void addEvents() {
+        white1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_BUTTON_PRESS){
+                    white1.setImageResource(R.drawable.pressed_white_key);
+                    Toast.makeText(MainActivity.this,"da press",Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void addControl() {
+        white1 = (ImageView) findViewById(R.id.white1);
+        white2 = (ImageView) findViewById(R.id.white2);
+        white3 = (ImageView) findViewById(R.id.white3);
+        white4 = (ImageView) findViewById(R.id.white4);
+        white5 = (ImageView) findViewById(R.id.white5);
+        white6 = (ImageView) findViewById(R.id.white6);
+        white7 = (ImageView) findViewById(R.id.white7);
+        black1 = (ImageView) findViewById(R.id.black1);
+        black2 = (ImageView) findViewById(R.id.black2);
+        black3 = (ImageView) findViewById(R.id.black3);
     }
 }
